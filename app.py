4ad104ac964398c5379ab04b7de20be8ea3725cc
bhavya7612 <2003bhavyajain@gmail.com>
2024-05-03 23:38:38 +0530

    second commit update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pickle
 import bz2
-from flask import Flask, request, jsonify, render_template, session,redirect
+from flask import Flask, request, render_template, session, redirect
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -25,14 +25,15 @@ dbcon = mongodbconnection(username='bhavya', password='bhamongo123')
 list_cursor = dbcon.getdata(dbName='FireDataML', collectionName='ml_task')
 log.info('Connected to Mongodb and data retrieved')
 
-# Data From MongoDB is used for Standardization
+# # Data From MongoDB is used for Standardization
 df = pd.DataFrame(list_cursor)
 df.drop('_id', axis=1, inplace=True)
 log.info('DataFrame created')
-scaler = StandardScaler()
 X = df.drop(['FWI', 'Classes'], axis=1)
 # Standardize
-X_reg_scaled = scaler.fit_transform(X)
+scaler = StandardScaler()
+# X_reg_scaled = scaler.fit_transform(X)
+scaler.fit_transform(X)
 log.info('Standardization done')
 
 obj_model=mysqlconnector()
@@ -42,6 +43,9 @@ def home():
     log.info('Home page loaded successfully')
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -93,26 +97,6 @@ def modelinput():
     else:
         return render_template('nologin.html')
 
-# Route for API Testing
-@app.route('/predict_api', methods=['POST'])
-def predict_api():
-    try:
-        data = request.json['data']
-        print(data)
-        log.info('Input from Api testing', data)
-        new_data = [list(data.values())]
-        final_data = scaler.transform(new_data)
-        output = int(model_C.predict(final_data)[0])
-        if output == 1:
-            text = 'The Forest in Danger'
-        else:
-            text = 'Forest is Safe'
-        return jsonify(text, output)
-    except Exception as e:
-        output = 'Check the input again!'
-        log.error('error in input from Postman', e)
-        return jsonify(output)
-
 
 # Route for Classification Model
 @app.route('/predict', methods=['POST'])
@@ -125,12 +109,12 @@ def predict():
         log.info('Prediction done for Classification model')
         if output == 0:
             text = 'Forest is Safe!'
-            return render_template('model-input.html', prediction_text1="{} ".format(text))
+            return render_template('output.html', prediction_text="{} ".format(text))
         text = 'Forest is in Danger!'
-        return render_template('model-input.html', prediction_text1="{} --- Fire can occur!".format(text))
+        return render_template('output29.html', prediction_text="{} --- Fire can occur!".format(text))
     except Exception as e:
         log.error('Input error, check input', e)
-        return render_template('model-input.html', prediction_text1="Check the Input again!!!")
+        return render_template('model-input.html', prediction_text="Check the Input again!!!")
 
 
 # Route for Regression Model
@@ -138,25 +122,24 @@ def predict():
 def predictR():
     try:
         data = [float(x) for x in request.form.values()]
+        print(data)
         data = [np.array(data)]
         data = scaler.transform(data)
+        print(data)
         output = model_R.predict(data)[0]
+        print(output)
         log.info('Prediction done for Regression model')
         if output > 29.0:
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Extremely hazardous. Fire occurrence very likely".format(output))
+            return render_template('output29.html', prediction_text="Fire Weather Index is {:.4f} ---- Extremely hazardous. Very high chances of fire occurrence".format(output))
         elif (output>=16.0 and output<=29.0):
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Fire occurrence more favourable".format(output))
-        elif (output>=8.0 and output<16.0):
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Fire occurrence favourable".format(output))
-        elif (output>=4.0 and output<8.0):
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Fire occurrence less favourable".format(output))
-        elif (output>=1.0 and output<4.0):
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Fire occurrence unfavourable".format(output))
+            return render_template('output16.html', prediction_text="Fire Weather Index is {:.4f} ---- Fire occurrence highly favourable".format(output))
+        elif (output>=1.0 and output<16.0):
+            return render_template('output1.html', prediction_text="Fire Weather Index is {:.4f} ---- Fire occurrence less favourable".format(output))
         else:
-            return render_template('model-input.html', prediction_text2="Fire Weather Index is {:.4f} ---- Safe.. Fire occurrence very low".format(output))
+            return render_template('output.html', prediction_text="Fire Weather Index is {:.4f} ---- Safe.. Almost no chance of fire occurrence".format(output))
     except Exception as e:
         log.error('Input error, check input', e)
-        return render_template('model-input.html', prediction_text2="Check the Input again!!!")
+        return render_template('model-input.html', prediction_text="Check the Input again!!!")
 
 
 # Run APP in Debug mode
